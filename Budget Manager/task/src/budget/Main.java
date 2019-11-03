30ec70351480eb6ab88ff100e89382315f9fb63f
Maciej <maciekqwertyzaqwsx@gmail.com>
2019-11-03 16:55:23 +0100

    compleated stage 1

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -1,7 +1,29 @@
 package budget;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> productList = new ArrayList<>();
+        double totalPrice = 0;
+
+        while(scanner.hasNextLine()) {
+            String product = scanner.nextLine();
+
+            double price = Double.parseDouble(product.split("\\$")[1]);
+
+            productList.add(product);
+            totalPrice += price;
+        }
+
+        for(String product : productList) {
+            System.out.println(product);
+        }
+
+        System.out.println("Total: $" + totalPrice);
     }
 }
